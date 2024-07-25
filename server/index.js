7458d28fbc1139cbade230fb2c31d32df71798f9
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-07-25 21:00:11 +0530

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const connectedDb = require("./config/db.config");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,12 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await connectedDb();
+  } catch (error) {
+    console.log(" error to connecting");
+  }
+
   console.log(`Server is running on port ${PORT}`);
 });
