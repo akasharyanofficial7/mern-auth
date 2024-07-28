b51c379283a658f441e4f567d80a06fadd9cc790
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-07-28 15:56:19 +0530

    create the signup page functionality vand added hot toast

--- client/project/src/pages/SignUp.jsx
+++ client/project/src/pages/SignUp.jsx
@@ -1,33 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { toast, Toaster } from "react-hot-toast";
+import axios from "axios";
 export default function SignUp() {
+  const [FormData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...FormData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        "http://localhost:5000/api/signup",
+        FormData
+      );
+
+      setFormData({
+        username: "",
+        email: "",
+        password: "",
+      });
+      console.log(res.data);
+      setLoading(false);
+      setError(false);
+      toast.success("User created successfully!");
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+      console.log(error);
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
+      <Toaster position="top-center" reverseOrder={false} />
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          value={FormData.username}
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          value={FormData.email}
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          value={FormData.password}
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          disabled={loading}
+        >
+          {loading ? "loading..." : "Sign up"}
         </button>
       </form>
+      {/* <p
+        className={`text-gray-500 p-2 text-center rounded-md mt-2 ${
+          error ? "bg-red-300  " : ""
+        }`}
+      >
+        {error && "Something went wrong"}
+      </p> */}
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to="/signin">
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,6 +5,9 @@ const signup = async (req, res, next) => {
   console.log(req.body);
   const { username, email, password } = req.body;
 
+  if (!password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
